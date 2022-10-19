dd5563a613f5d243c846922731d653d66d6dc2ac
NAYEEM65 <mathnayeem054@gmail.com>
2022-10-20 01:05:45 +0600

    registration functionality added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header/Header';
+import app from './firebase/config';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Reset from './pages/auth/Reset';
 import Contact from './pages/Contact/Contact';
 import Home from './pages/Home/Home';
+import { ToastContainer } from 'react-toastify';
 function App() {
+    console.log(app);
     return (
         <Router>
             <Header />
@@ -17,6 +20,7 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/reset" element={<Reset />} />
             </Routes>
+            <ToastContainer />
             <Footer />
         </Router>
     );
