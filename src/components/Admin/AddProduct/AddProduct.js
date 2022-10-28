26d85f876c5997790e46b47e95c927034a8823a7
NAYEEM65 <mathnayeem054@gmail.com>
2022-10-28 08:39:00 +0600

    viewproducts page designed

--- src/components/Admin/AddProduct/AddProduct.js
+++ src/components/Admin/AddProduct/AddProduct.js
@@ -4,6 +4,8 @@ import { db, storage } from '../../../firebase/config';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { toast } from 'react-toastify';
 import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+import Loader from '../../Loader/Loader';
 
 const categories = [
     { id: 1, name: 'Laptop' },
@@ -17,13 +19,15 @@ const AddProduct = () => {
         name: '',
         imageUrl: '',
         price: '',
+        stock: '',
         category: '',
         brand: '',
         desc: '',
     };
-    const [product, setProduct] = useState(initialState);
+    const [product, setProduct] = useState({ ...initialState });
     const [uploadProgress, setUploadProgress] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -72,6 +76,7 @@ const AddProduct = () => {
                 name: product.name,
                 imageUrl: product.imageUrl,
                 price: Number(product.price),
+                stock: Number(product.stock),
                 category: product.category,
                 brand: product.brand,
                 desc: product.desc,
@@ -79,134 +84,153 @@ const AddProduct = () => {
             });
             setIsLoading(false);
             toast.success('Product Added successfully');
-            setProduct(initialState);
+            setUploadProgress(0);
+            setProduct({ ...initialState });
+            navigate('/admin/all-products');
         } catch (error) {
             setIsLoading(false);
             console.log(error.message);
         }
     };
     return (
-        <div>
-            <h1>Add Product</h1>
-            <form onSubmit={productSubmit}>
-                <Card cardClass={'w-full max-w-[500px] p-4'}>
-                    <div className="flex flex-col gap-2 mb-2 items-start justify-start">
-                        <label> Product Name:</label>
-                        <input
-                            type="text"
-                            placeholder="Product Name"
-                            required
-                            name="name"
-                            value={product.name}
-                            className="rounded w-full bg-gray-200"
-                            onChange={(e) => handleInputChange(e)}
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2 mb-2 border-[1px] border-gray-300 rounded p-2 items-start justify-start w-full">
-                        <label> Product Image:</label>
-                        <div className={'p-4 w-full'}>
-                            {uploadProgress === 0 ? null : (
-                                <div className="text-white animate-pulse w-full bg-gray-300 rounded-full">
-                                    <div
-                                        className={`bg-blue-500 rounded-full text-white px-4 mb-3 w-[${uploadProgress}%]`}
-                                    >
-                                        {uploadProgress < 100
-                                            ? `Uploading ${uploadProgress}%`
-                                            : `Upload progress ${uploadProgress}%`}
+        <>
+            {isLoading && <Loader />}
+            <div>
+                <h1 className="text-4xl w-fit font-bold text-slate-800 border-b-2 border-gray-400 ">
+                    Add Product
+                </h1>
+                <form onSubmit={productSubmit}>
+                    <Card cardClass={'w-full max-w-[500px] p-4'}>
+                        <div className="flex flex-col gap-2 mb-2 items-start justify-start">
+                            <label> Product Name:</label>
+                            <input
+                                type="text"
+                                placeholder="Product Name"
+                                required
+                                name="name"
+                                value={product.name}
+                                className="rounded w-full bg-gray-200"
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2 mb-2 border-[1px] border-gray-300 rounded p-2 items-start justify-start w-full">
+                            <label> Product Image:</label>
+                            <div className={'p-4 w-full'}>
+                                {uploadProgress === 0 ? null : (
+                                    <div className="text-white animate-pulse w-full bg-gray-300 rounded-full">
+                                        <div
+                                            className={`bg-blue-500 rounded-full text-white px-4 mb-3 w-[${uploadProgress}%]`}
+                                        >
+                                            {uploadProgress < 100
+                                                ? `Uploading ${uploadProgress}%`
+                                                : `Upload progress ${uploadProgress}%`}
+                                        </div>
                                     </div>
-                                </div>
-                            )}
-
-                            <div className="flex flex-col gap-2">
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    placeholder="Product Image"
-                                    required
-                                    name="image"
-                                    className="rounded w-full bg-gray-200"
-                                    onChange={(e) => handleImageChange(e)}
-                                />
-                                {product.imageUrl && (
-                                    <span className="relative">
-                                        <input
-                                            type="text"
-                                            placeholder="image url"
-                                            value={product.imageUrl}
-                                            name="imageUrl"
-                                            disabled
-                                            className="rounded w-full bg-gray-200"
-                                        />
-                                    </span>
                                 )}
+
+                                <div className="flex flex-col gap-2">
+                                    <input
+                                        type="file"
+                                        accept="image/*"
+                                        placeholder="Product Image"
+                                        required
+                                        name="image"
+                                        className="rounded w-full bg-gray-200"
+                                        onChange={(e) => handleImageChange(e)}
+                                    />
+                                    {product.imageUrl && (
+                                        <span className="relative">
+                                            <input
+                                                type="text"
+                                                placeholder="image url"
+                                                value={product.imageUrl}
+                                                name="imageUrl"
+                                                disabled
+                                                className="rounded w-full bg-gray-200"
+                                            />
+                                        </span>
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex flex-col gap-2 mb-2 items-start justify-start">
-                        <label> Product Price:</label>
-                        <input
-                            type="number"
-                            placeholder="Product Price"
-                            required
-                            name="price"
-                            value={product.price}
-                            className="rounded w-full bg-gray-200"
-                            onChange={(e) => handleInputChange(e)}
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2 mb-2 items-start justify-start">
-                        <label> Product Category:</label>
-                        <select
-                            name="category"
-                            value={product.category}
-                            required
-                            onChange={(e) => handleInputChange(e)}
+                        <div className="flex flex-col gap-2 mb-2 items-start justify-start">
+                            <label> Product Price:</label>
+                            <input
+                                type="number"
+                                placeholder="Product Price"
+                                required
+                                name="price"
+                                value={product.price}
+                                className="rounded w-full bg-gray-200"
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2 mb-2 items-start justify-start">
+                            <label> Stock:</label>
+                            <input
+                                type="number"
+                                placeholder="Product stock"
+                                required
+                                name="stock"
+                                value={product.stock}
+                                className="rounded w-full bg-gray-200"
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2 mb-2 items-start justify-start">
+                            <label> Product Category:</label>
+                            <select
+                                name="category"
+                                value={product.category}
+                                required
+                                onChange={(e) => handleInputChange(e)}
+                            >
+                                <option value="" disabled>
+                                    -- choose product category --
+                                </option>
+                                {categories.map((cat) => {
+                                    return (
+                                        <option key={cat.id} value={cat.name}>
+                                            {cat.name}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </div>
+                        <div className="flex flex-col gap-2 mb-2 items-start justify-start">
+                            <label>Product Company/Brand:</label>
+                            <input
+                                type="text"
+                                placeholder="Product Brand"
+                                required
+                                name="brand"
+                                value={product.brand}
+                                className="rounded w-full bg-gray-200"
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </div>
+                        <div className="flex flex-col gap-2 mb-2 items-start justify-start">
+                            <label>Product Description: </label>
+                            <textarea
+                                cols="30"
+                                rows="10"
+                                required
+                                name="desc"
+                                value={product.desc}
+                                className="rounded w-full bg-gray-200"
+                                onChange={(e) => handleInputChange(e)}
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="bg-blue-600 text-white px-3 py-2 w-full transition duration-100 ease-in-out rounded hover:bg-7lue-600"
                         >
-                            <option value="" disabled>
-                                -- choose product category --
-                            </option>
-                            {categories.map((cat) => {
-                                return (
-                                    <option key={cat.id} value={cat.name}>
-                                        {cat.name}
-                                    </option>
-                                );
-                            })}
-                        </select>
-                    </div>
-                    <div className="flex flex-col gap-2 mb-2 items-start justify-start">
-                        <label>Product Company/Brand:</label>
-                        <input
-                            type="text"
-                            placeholder="Product Brand"
-                            required
-                            name="brand"
-                            value={product.brand}
-                            className="rounded w-full bg-gray-200"
-                            onChange={(e) => handleInputChange(e)}
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2 mb-2 items-start justify-start">
-                        <label>Product Description: </label>
-                        <textarea
-                            cols="30"
-                            rows="10"
-                            required
-                            name="desc"
-                            value={product.desc}
-                            className="rounded w-full bg-gray-200"
-                            onChange={(e) => handleInputChange(e)}
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="bg-blue-600 text-white px-3 py-2 w-full transition duration-100 ease-in-out rounded hover:bg-7lue-600"
-                    >
-                        Save Product
-                    </button>
-                </Card>
-            </form>
-        </div>
+                            Save Product
+                        </button>
+                    </Card>
+                </form>
+            </div>
+        </>
     );
 };
 
--- src/components/Admin/ViewProducts/ViewProducts.js
+++ src/components/Admin/ViewProducts/ViewProducts.js
@@ -1,7 +1,120 @@
-import React from 'react';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { db } from '../../../firebase/config';
+import Loader from '../../Loader/Loader';
+import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const ViewProducts = () => {
-    return <div>ViewProducts</div>;
+    const [products, setProducts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const fetchProducts = async () => {
+        setIsLoading(true);
+        try {
+            const productsRef = collection(db, 'products');
+            const q = query(productsRef, orderBy('createdAt', 'desc'));
+            onSnapshot(q, (snapshot) => {
+                const allProducts = snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                setProducts(allProducts);
+                setIsLoading(false);
+            });
+        } catch (error) {
+            setIsLoading(false);
+            toast.error(error.message);
+        }
+    };
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    return (
+        <>
+            {isLoading && <Loader />}
+            <div>
+                <h2 className="text-3xl text-slate-700 font-bold border-b-2 border-gray-400 w-fit">
+                    All Products
+                </h2>
+                {products.length === 0 ? (
+                    <h2>No Product found</h2>
+                ) : (
+                    <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+                        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="py-3 px-6">
+                                        Serial
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Image
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Category
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Price
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Stock
+                                    </th>
+                                    <th scope="col" class="py-3 px-6">
+                                        Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {products.map((product, index) => (
+                                    <tr
+                                        className='"bg-white border-b dark:bg-gray-900 dark:border-gray-700"'
+                                        key={product.id}
+                                    >
+                                        <td className="py-4 px-6">{index + 1}</td>
+                                        <td className="py-4 px-6">
+                                            <img
+                                                src={product.imageUrl}
+                                                alt={product.id}
+                                                className="h-[60px]"
+                                            />
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            {product.name.slice(0, 20)}...
+                                        </td>
+                                        <td className="py-4 px-6">{product.category}</td>
+                                        <td className="py-4 px-6">${product.price}</td>
+                                        <td className="py-4 px-6">
+                                            <span
+                                                className={`px-3 py-1 rounded ${
+                                                    product.stock > 0
+                                                        ? 'bg-green-300'
+                                                        : 'bg-orange-300'
+                                                }`}
+                                            >
+                                                {product.stock}
+                                            </span>
+                                        </td>
+                                        <td className="py-4 px-6">
+                                            <span className="flex justify-around items-center">
+                                                <Link to="/admin/add-product">
+                                                    <AiOutlineEdit className="text-green-500 text-xl cursor-pointer" />
+                                                </Link>
+                                                <AiOutlineDelete className="text-orange-500 text-xl cursor-pointer" />
+                                            </span>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default ViewProducts;
