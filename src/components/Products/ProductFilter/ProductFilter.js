f14a1ccdfa8fd2d424854a0a178cad4885f875c0
NAYEEM65 <mathnayeem054@gmail.com>
2022-10-28 23:28:15 +0600

    product components created

---
+++ src/components/Products/ProductFilter/ProductFilter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProductFilter = () => {
+    return <div>products filter</div>;
+};
+
+export default ProductFilter;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Products from '../../components/Products/Products';
 import Slider from '../../components/Slider/Slider';
 
 const Home = () => {
     return (
         <div>
-            {/* <Slider /> */}
-            <h1>Home</h1>
+            <Slider />
+            <Products />
         </div>
     );
 };
